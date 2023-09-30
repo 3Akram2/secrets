490860c4b6c4495d73b8073a5e02e81d1eb00ac5
Omar Akram <omar@Omars-Laptop.local>
2023-10-01 02:40:47 +0300

    hashing using bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import User from "./models/userModel.js";
-import encrypt from "mongoose-encryption";
+import bcrypt from "bcrypt";
+const saltRounds = 10;
 const app = express();
 const PORT = 3000;
 mongoose.connect("mongodb://localhost:27017/userDB")
@@ -15,28 +16,31 @@ app.set('view engine','ejs')
 app.get('/',function(req,res){
     res.render('home')
 });
-app.get('/login',function(req,res){
+app.get('/login',(req,res)=>{
     res.render('login')
 });
-app.get('/register',function(req,res){
+app.get('/register',(req,res)=>{
     res.render('register')
 })
-app.post("/register",function(req,res){
-    console.log(req.body)
-const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-});
-console.log(`newUser.email --> ${newUser.email}`);
-console.log(`newUser.password --> ${newUser.password}`);
+app.post("/register",(req,res)=>{
+    console.log(req.body);
+    bcrypt.hash(req.body.password,saltRounds,(err,hash)=>{
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+    });
+    console.log(`newUser.email --> ${newUser.email}`);
+    console.log(`newUser.password --> ${newUser.password}`);
+    
+    try {
+        newUser.save()
+         console.log("new user saved successfully");
+         res.render("secrets")
+    } catch (error) {
+      console.log(err)  
+    }
+    })
 
-try {
-    newUser.save()
-     console.log("new user saved successfully");
-     res.render("secrets")
-} catch (error) {
-  console.log(err)  
-}
 
 
 })
@@ -45,9 +49,14 @@ app.post('/login', async (req,res) => {
     try {
       const loginUser = await User.findOne({ email: username });
       console.log({ loginUser });
-      if (loginUser.password !== password) {
-        throw new Error("Password mismatch!");
-      }
+      // if (loginUser.password !== password) {
+      //   throw new Error("Password mismatch!");
+      // }
+      bcrypt.compare(password,loginUser.password,(err,result)=>{
+        if (result!== true) {
+          throw new Error("Password mismatch!");
+        }
+      })
       console.log("loginUser.password",loginUser.password)
       res.render("secrets")
     } catch (error) {
